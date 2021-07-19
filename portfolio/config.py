7fea56733abda1869097702fb18729c7daeec5ed
Ritwik Ranjan <ritwikranjan99@gmail.com>
2021-07-19 17:13:01 +0530

    Delete config.py

--- portfolio/config.py
+++
@@ -1,7 +0,0 @@
-MONGODB_HOST = "mongodb+srv://doadmin:581P2typ047Ldmq3@test-django-26062983q-fc07696a.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=test-django-26062983q&tls=true&tlsCAFile=ca-certificate.crt"
-
-AWS_ACCESS_KEY_ID = 'AKIAXZ6ACCEH2WC2HAPC'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'portfolio-ritwik'
-
-SECRET_KEY = 'django-secure-d=l#kty3+&sdf)0*&q_gj@dakfh*1^fr8+4td@*ao(vj=)0u552(z'
